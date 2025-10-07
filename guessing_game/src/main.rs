6cd5c1912440cdf88994899ef85dfb06fbe02ef5
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-10-07 17:36:19 +0600

    Guessing Game - Comparing the Guess to the Secret Number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,17 @@
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    // Suggested but gives `deprecated` warnings
+    // let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -12,4 +22,11 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    // https://doc.rust-lang.org/stable/book/ch02-00-guessing-game-tutorial.html#comparing-the-guess-to-the-secret-number
+//     match guess.cmp(&secret_number) {
+//         Ordering::Less => println!("Too small!"),
+//         Ordering::Equal => println!("You Win!"),
+//         Ordering::Greater => println!("Too big!"),
+//     }
 }
