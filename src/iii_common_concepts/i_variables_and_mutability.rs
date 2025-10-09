92aee723d605075bedfa7a721f8dc8f9501cf242
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-10-09 21:32:54 +0600

    Chapter 3.1 - Shadowing

--- src/iii_common_concepts/i_variables_and_mutability.rs
+++ src/iii_common_concepts/i_variables_and_mutability.rs
@@ -26,3 +26,16 @@ pub fn constants() {
     );
     println!("The speed of light is {} m/s", SPEED_OF_LIGHT);
 }
+
+pub fn shadowing() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     i_variables_and_mutability::mutability_broken();
     i_variables_and_mutability::mutability_fixed();
     i_variables_and_mutability::constants();
+    i_variables_and_mutability::shadowing();
 }
