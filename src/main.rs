b7014db6fabfcf66be810ae56106b2aa67061655
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-10-14 20:23:49 +0600

    Chapter 3.3 - Control Flow - if Expressions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ pub mod iii_common_concepts;
 
 // use iii_common_concepts::i_variables_and_mutability;
 // use iii_common_concepts::ii_data_types;
-use iii_common_concepts::iii_functions;
+// use iii_common_concepts::iii_functions;
+use iii_common_concepts::v_control_flow;
 
 fn main() {
     // Chapter 3.1 - Variables and Mutability
@@ -26,6 +27,11 @@ fn main() {
     // iii_functions::with_parameter(5);   // here `5` is the function argument
     // iii_functions::multiple_parameters(5, 'h');
     // iii_functions::statements_and_expression();
-    println!("{}", iii_functions::five());
-    println!("{}", iii_functions::plus_one(5));
+    // println!("{}", iii_functions::five());
+    // println!("{}", iii_functions::plus_one(5));
+
+    // Chapter 3.4 - Control Flow
+    v_control_flow::if_expressions::basic();
+    v_control_flow::if_expressions::else_if();
+    v_control_flow::if_expressions::using_if_in_a_let_statement();
 }
